9e31b1b6adbba04e19fb1f90e50e5e9913325968
AbdullhGaber <abdullh760@yahoo.com>
2026-08-17 16:29:07 +0300

    fix: fix bottom nav overlapping with device gestures

--- design_system/src/main/java/com/iti/mongez/org/designsystem/components/navigation/AppNavigationBar.kt
+++ design_system/src/main/java/com/iti/mongez/org/designsystem/components/navigation/AppNavigationBar.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -64,6 +65,7 @@ fun AppNavigationBar(
     Surface(
         modifier = modifier
             .fillMaxWidth()
+            .navigationBarsPadding()
             .padding(horizontal = Theme.spacing.xl, vertical = Theme.radius.xxl)
             .mongezShadow(
                 color = Theme.colorScheme.brand.primary.copy(alpha = 0.58f),
